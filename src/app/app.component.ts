b9b37b3853a68dbb3938eecb5d019d66dbd81b64
terez50 <nagy.peterne.terez@gmail.com>
2022-02-14 20:10:04 +0100

    01.27-i állapot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-components-pipes';
+  title = 'Data Binding example';
+
+  btnDisabled: boolean = false;
+
+  onSwitchDisabled(): void {
+    this.btnDisabled = !this.btnDisabled;
+  }
 }
